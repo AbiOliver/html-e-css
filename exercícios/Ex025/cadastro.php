6e7dd56caaf7ae30cf8edbf3eeabc6c77930a1ab
Abimeleque Oliveira <52713162+AbiOliver@users.noreply.github.com>
2022-08-03 16:25:01 -0300

    racunho

--- exercícios/Ex025/cadastro.php
+++ exercícios/Ex025/cadastro.php
@@ -1,2 +1,26 @@
-Dados enviado. 
-Aprenda php para saber o que fazer com eles.
+<?php
+if(isset($_POST['email']) && !empty($_POST['email'])){
+
+
+
+$nome = addcslashes($_POST['name']);
+$nome = addcslashes($_POST['email']);
+$nome = addcslashes($_POST['message']);
+
+
+$to = "abimeleque15@gmail.com";
+$subject = "Contato - BolosDaJu";
+
+$body = "Nome: ".$nome. "\n ".
+        "Email: ".$email. "\n".
+        "Mensagem: ".$mensagem; 
+$header = "from: abimeleque15@gmail.com"."\r\n"."Reply-To: ".$email."\e\n"."X=Mailer:PHP/".phpversion
+if(mail($to,$subject,$body,$header)){
+
+    echo("email enviado com sucesso! ");
+}else{
+    echo("o email não pode ser enviado"); 
+}
+
+}
+?>
